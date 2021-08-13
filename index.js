ac7c6daa5985b1d5a6543db0dd264036c59b66be
Alexander Bibikov <abibikov.com@gmail.com>
2021-08-13 13:28:04 +0300

    modified to class

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const formatPrice = (price, currency, digits) => {
-	let formatter = new Intl.NumberFormat("ru", {
-		style: "currency",
-		currency: currency,
-		minimumFractionDigits: digits
-	});
+export default class Formatter {
+	constructor (locale = 'ru-RU', currency = "RUB", currencyType = "symbol") {
+		this.locale = locale
+		this.currency = currency
+		this.currencyType = currencyType
+	}
 
-	return formatter.format(price);
+	formatPrice (price, digits = 0, locale = "ru-RU", currency = "RUB", currencyType = "symbol") {
+		let formatter = new Intl.NumberFormat(locale, {
+			style: "currency",
+			currency: currency,
+			currencyDisplay: currencyType,
+			minimumFractionDigits: digits
+		});
+
+		return formatter.format(price);
+	}
+
+	formatDate = (value) => {
+		return value
+	}
+
+	formatPhone = (value) => {
+		return value
+	}
+
+	formatEmail = (value) => {
+		return value
+	}
 }
-
-export default formatPrice
