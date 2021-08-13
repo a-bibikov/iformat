4e5e4ff50bb2fbc8004b36b9d44e4ba782497079
Alexander Bibikov <abibikov.com@gmail.com>
2021-08-13 14:11:44 +0300

    set class variables

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 export default class Formatter {
-	constructor (locale = 'ru-RU', currency = "RUB", currencyType = "symbol") {
-		this.locale = locale
-		this.currency = currency
-		this.currencyType = currencyType
-	}
+    constructor (locale = 'ru-RU', currency = "RUB", currencyType = "symbol") {
+        this.locale = locale
+        this.currency = currency
+        this.currencyType = currencyType
+    }
 
-	formatPrice (price, digits = 0, locale = "ru-RU", currency = "RUB", currencyType = "symbol") {
-		let formatter = new Intl.NumberFormat(locale, {
-			style: "currency",
-			currency: currency,
-			currencyDisplay: currencyType,
-			minimumFractionDigits: digits
-		});
+    formatPrice (price, digits = 0, locale = this.locale, currency = this.currency, currencyType = this.currencyType) {
+        let formatter = new Intl.NumberFormat(locale, {
+            style: "currency",
+            currency: currency,
+            currencyDisplay: currencyType,
+            minimumFractionDigits: digits
+        });
 
-		return formatter.format(price);
-	}
+        return formatter.format(price);
+    }
 
-	formatDate = (value) => {
-		return value
-	}
+    formatDate = (value) => {
+        return value
+    }
 
-	formatPhone = (value) => {
-		return value
-	}
+    formatPhone = (value) => {
+        return value
+    }
 
-	formatEmail = (value) => {
-		return value
-	}
+    formatEmail = (value) => {
+        return value
+    }
 }
